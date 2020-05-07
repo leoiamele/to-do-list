f4e5d9d3e2017e258c7726a16036311761e925e2
Leandro Iamele <leandroiamele@gmail.com>
2020-05-07 17:44:39 -0300

    Ceate UI selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.getElementById('filter');
+const taskInput = document.getElementById('task');
