b73494c0c1137ea8de6af265b0aa6623fa013cf8
Leandro Iamele <leandroiamele@gmail.com>
2020-05-08 11:41:10 -0300

    Remove tasks from the to do list

--- app.js
+++ app.js
@@ -7,28 +7,34 @@ const taskInput = document.getElementById('task');
 loadEventListener();
 
 function loadEventListener(){
-  form.addEventListener('submit', addTask)
+  form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTasks);
 }
 
 function addTask(e){
-  
   if(taskInput.value === ''){
     alert('Necesitas agregar una tarea');
   } else {
   const li = document.createElement('li');
-  
   li.className = 'collection-item';
   li.appendChild(document.createTextNode(taskInput.value));
   const link = document.createElement('a');
   link.className='delete-item secondary-content';
   link.innerHTML = '<i class="fa fa-remove" ></i>';
-  
   li.appendChild(link);
   taskList.appendChild(li);
   taskInput.value = '';
   console.log(li);
   }
   e.preventDefault();
+}
+
+function removeTasks(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    e.target.parentElement.parentElement.remove();
+  }
+  
+    
 }
 
 
