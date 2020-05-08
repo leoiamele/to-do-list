f9c14854212ce4967536f3fb2211cb51cd454b50
Leandro Iamele <leandroiamele@gmail.com>
2020-05-08 10:48:45 -0300

    Add tasks to the todo list

--- app.js
+++ app.js
@@ -3,3 +3,34 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.getElementById('filter');
 const taskInput = document.getElementById('task');
+
+loadEventListener();
+
+function loadEventListener(){
+  form.addEventListener('submit', addTask)
+}
+
+function addTask(e){
+  
+  if(taskInput.value === ''){
+    alert('Necesitas agregar una tarea');
+  } else {
+  const li = document.createElement('li');
+  
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  const link = document.createElement('a');
+  link.className='delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove" ></i>';
+  
+  li.appendChild(link);
+  taskList.appendChild(li);
+  taskInput.value = '';
+  console.log(li);
+  }
+  e.preventDefault();
+}
+
+
+
+
